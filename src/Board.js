a98616a8d659d0e028f5408ab27b60bcb184888e
zhangchunxia <zcx1458@163.com>
2018-05-14 17:11:42 +0800

    step 5

--- src/Board.js
+++ src/Board.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import Chesspiece from './Chesspiece';
 import './Board.css';
-
+/*棋盘*/
 class Board extends React.Component {
     constructor(props) {
       super(props);
       this.state= {
-        squares: Array(9).fill(''),
-        xIsNext:true,
+        squares: Array(9).fill(''), //方格的状态存储
+        xIsNext:true, 
       }
     }
+    
     handClick(index) {
      const squares=this.state.squares.slice();
-     if(this.win(squares)||squares[index] ){
+     if(this.win(squares)||squares[index] ){ //点过的方格和已经连成线的不再改变
        return;
      }
-     squares[index]=this.state.xIsNext?'x':'O';
+     squares[index]=this.state.xIsNext?'x':'O'; //设置被点击的方格的状态
      this.setState({
        squares:squares,
        xIsNext:!this.state.xIsNext
       });
+
     }
-    win(squares) {
+    checkArray(para,arr) {
+        let processArr = arr.filter(function(value) {
+            return value == para;
+        });
+    
+        return processArr.length;  // 这里返回数组长度
+      }
+    win(squares) {//判断输赢情况
       const lines=[
         [0,1,2],
         [3,4,5],
@@ -32,6 +41,9 @@ class Board extends React.Component {
         [0,4,8],
         [2,4,6],
       ];
+      
+      const isClick= this.checkArray("",squares);
+      if(isClick>4) return;
       for(let i=0;i<lines.length;i++){
         const [a,b,c]=lines[i];
         if(squares[a] && squares[a]===squares[b]&&
@@ -44,6 +56,7 @@ class Board extends React.Component {
     render() {
       const winner=this.win(this.state.squares);
       let  status;
+      let count=0;
       if(winner){
         status='winner';
       }else{
